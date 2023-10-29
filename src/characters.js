33a9d77f0f8e5b30dddabb94b60f41a31f875743
babsthegoalie <bzkondracki@gmail.com>
2023-10-29 17:42:20 -0600

    additions to dummy database

--- src/characters.js
+++ src/characters.js
@@ -1,57 +1,73 @@
-// database of pets, pets breed, pet age, owners name , telephone numbers, and appointments
+// database of pcharacters
 const characters = [
     {
         id: 1,
-        name: 'Fido',
-        breed: 'Labrador',
-        age: 3,
-        owner: 'John',
-        telephone: '555-555-5555',
-        appointments: [
+        name: 'Albus Dumbledore',
+        house: 'Gryffindor',
+        role: 'faculty',
+        sex: 'm',
+        summary: 'Professor Albus Percival Wulfric Brian Dumbledore, Headmaster of Hogwarts School of Witchcraft and Wizardry.',
+        profilePicture: '/assets/gryffindor/Albus_Dumbledore.webp'
+        numberOfReviews: 'x',
+        averageReview: 'x',
 
-            {
-                date: '01/01/2020',
-                time: '10:00',
-                reason: 'checkup'
-            },
-        ]
+        
     },
     {
         id: 2,
-        name: 'Spot',
-        breed: 'Poodle',
-        age: 2,
-        owner: 'Jane',
-        telephone: '555-555-5555',
-        appointments: [
-            {
-                date: '01/01/2020',
-                time: '10:00',
-                reason: 'checkup'
-            },
+        name: 'Fred Weasley',
+        house: 'Gryffindor',
+        role: 'student',
+        sex: 'm',
+        summary: 'Fred Weasley was an English pure-blood wizard, was the fourth son and the middle child, the most daring and dominant among the twins of Arthur Weasley and Molly Weasley, younger brother to Bill, Charlie and Percy, older twin brother and best friend to George Weasley, and older brother of Ron and Ginny Weasley. Both he and his twin brother were popular students, known for their sense of humour, pranks, inventions and the fact that they were Beaters for the Gryffindor Quidditch team.',
+        profilePicture: '/assets/gryffindor/Fred_Weasley.webp'
+        numberOfReviews: 'x',
+        averageReview: 'x',
 
-        ]
+        
     },
+
     {
         id: 3,
-        name: 'Rover',
-        breed: 'Pitbull',
-        age: 1,
-        owner: 'John',
-        telephone: '555-555-5555',
-        appointments: [
-            {
-                date: '01/01/2020',
-                time: '10:00',
-                reason: 'checkup'
-            },
-            {
-                date: '01/01/2020',
-                time: '10:00',
-                reason: 'checkup'
-            }
-        ]
-    }
+        name: 'George Weasley',
+        house: 'Gryffindor',
+        role: 'student',
+        sex: 'm',
+        summary: 'George Weasley was an English pure-blood wizard, was the fifth son and the middle child, the most daring and less dominant among the twins of Arthur Weasley and Molly Weasley, younger brother to Bill, Charlie and Percy, younger twin brother and best friend to Fred Weasley, and older brother of Ron and Ginny Weasley. Both he and his twin brother were popular students, known for their sense of humour, pranks, inventions and the fact that they were Beaters for the Gryffindor Quidditch team.',
+        profilePicture: '/assets/gryffindor/George_Weasley.webp'
+        numberOfReviews: 'x',
+        averageReview: 'x',
+
+        
+    },
+
+    {
+        id: 4,
+        name: 'Ginny Weasley',
+        house: 'Gryffindor',
+        role: 'student',
+        sex: 'f',
+        summary: 'Ginny Weasley was an English pure-blood witch, the only daughter of Arthur and Molly Weasley and the younger sister of Bill, Charlie, Percy, the late Fred, George and Ron. She was the first female to be born into the Weasley line for several generations. Ginny attended Hogwarts School of Witchcraft and Wizardry a year beneath the famous Harry Potter, and was sorted into Gryffindor House. During her first year, she developed a long-standing crush on Harry Potter and came under the influence of the memory of Tom Riddle's sixteen-year-old self, who was preserved in a diary, which forced Ginny to re-open the Chamber of Secrets, endangering the lives of many students, including her own.',
+        profilePicture: '/assets/gryffindor/Ginny_Weasley.webp'
+        numberOfReviews: 'x',
+        averageReview: 'x',
+
+        
+    },
+    
+    {
+        id: 5,
+        name: 'Harry Potter',
+        house: 'Gryffindor',
+        role: 'student',
+        sex: 'm',
+        summary: 'Harry Potter is a wizard who survived an attempted murder by an evil dark wizard named Lord Voldemort as an infant, leaving him with a distinctive lightning-shaped scar on his forehead. Harry's survival, coupled with the prophecy that surrounded him and Voldemort led him revered as one of the legendary figures by many in the Wizarding World as the Chosen One who is prophesied to defeat the Dark Lord.',
+        profilePicture: '/assets/gryffindor/Harry_Potter.jpeg'
+        numberOfReviews: 'x',
+        averageReview: 'x',
+
+        
+    },
 ];
 
 module.exports = characters;
