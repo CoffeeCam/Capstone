7043559e3a7114120b4dde463b0d1da1df49090d
CoffeeCam <cameron93malone@gmail.com>
2023-11-01 17:53:46 -0700

    ignore2

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import NavBar from './components/NavBar.jsx';
+import './App.css';
+import Login from './components/Login.jsx';
 
-const House = ({house, setSelectedHouse}) => {
-  console.log(house)
-  return (
-  <div className={house} onClick={() => setSelectedHouse(house)}></div>
-  )
-}
 const App = () => {
-
-  const [seclectedHouse, setSelectedHouse] = useState("")
-
   return (
-    <div id="container">
-      <div id="navbar">
-        <header>Hogwarts Rank-a-thon</header>
-        <div>House</div>
-        <div className={seclectedHouse}>{seclectedHouse}</div>
-      </div>
-      <div id="houses">
-          <House house="Gryffindor" setSelectedHouse={setSelectedHouse}/>
-          <House house="Hufflepuff" setSelectedHouse={setSelectedHouse}/>
-          <House house="Ravenclaw" setSelectedHouse={setSelectedHouse}/>
-          <House house="Slytherin" setSelectedHouse={setSelectedHouse}/>
+    <div>
+      <NavBar />
+      <div className="main-content">
+        <h2>Main Content</h2>
+        {/* Add your main content here */}
       </div>
     </div>
   );
