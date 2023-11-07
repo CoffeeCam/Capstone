e4ba49dcac9365b9c2085403317e33d30d55baa1
CoffeeCam <cameron93malone@gmail.com>
2023-11-06 18:27:32 -0800

    home

--- src/client/components/HomeTest.jsx
+++ src/client/components/HomeTest.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
-
-
+import React, { useState } from 'react';
 const HomeTest = () => {
+  const [categoryList, setCategoryList] = useState([]);
+  const handleImageClick = async (selectedCategory) => {
+    try {
+      const response = await fetch(`/api/=${selectedCategory}`);
+      const categoryData = await response.json();
+      setCategoryList(categoryData);
+    } catch (error) {
+      console.error('Error fetching category data:', error);
+    }
+  };
     return (
-        <h1>Main Content Here</h1>
-      );
-    };
-
+      <div>
+      <div className="main-content">
+        <h2>Main Content</h2>
+        <div>
+            <img
+            src="./src/client/assets/gryffindor/crest-gryffindor.png"
+            // alt=“Gryffindor”
+          onClick={() => handleImageClick('gryffindor')}/>
+        </div>
+        <div>
+            <img src="./src/client/assets/hufflepuff/crest-hufflepuff.png"
+            // alt=“Hufflepuff”
+            onClick={() => handleImageClick('hufflepuff')}/>
+        </div>
+        <div>
+            <img src="./src/client/assets/ravenclaw/crest-ravenclaw.png"
+            // alt=“Ravenclaw”
+            onClick={() => handleImageClick('ravenclaw')}/>
+        </div>
+        <div>
+            <img src="./src/client/assets/slytherin/crest-slytherin.png"
+            // alt=“Slytherin”
+            onClick={() => handleImageClick('slytherin')}/>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default HomeTest;
