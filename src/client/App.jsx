cf44792569622681c938c5ee54ad69382668d6eb
CoffeeCam <cameron93malone@gmail.com>
2023-11-13 18:29:44 -0800

    dark mode correction

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import NavBar from './components/NavBar.jsx';
 import HomeTest from './components/HomeTest.jsx';
 import SignupPage from '/src/client/components/SignupPage.jsx';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-
 const App = () => {
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+    document.body.classList.toggle('dark-mode', !isDarkMode);
+  };
+
   return (
     <div>
       <NavBar />
-      <div className="main-content">
-        
+      <div className={`main-content ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+        <button onClick={toggleDarkMode}>
+          {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+        </button>
         <Routes>
           <Route path='/' element={<HomeTest />} />
           <Route path='/signup' element={<SignupPage />} />
-          
         </Routes>
       </div>
     </div>
