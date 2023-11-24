34bc829dbd2fb3affd854c837e302e2eb51b9d5f
kalpravi1989 <kalpravi1989@gmail.com>
2023-11-24 12:07:46 -0600

    nav bar change done

--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -9,12 +9,12 @@ const NavBar = ({userId,isAdmin}) => {
     <div className="navbar">
       <h1>Hogwarts Rank-a-Thon</h1>
       {userId && isAdmin==false&&<Link to="/me"><button>User</button></Link>}
-      {isAdmin&&<Link to="/allUser">All Users</Link>}
+     
     {(userId)?(<Link to="/logout"> <button>Logout</button></Link>):(<Link to="/login"> <button>Login</button></Link>)}
    
     </div>
     <div>
-      <NavLinks />
+      <NavLinks userId={userId} isAdmin={isAdmin}/>
     </div>
 
     </>
